509baf619d5d2d1d68fe74bd88862d1271126bd2
AdrielVaz <158211788+AdrielVaz@users.noreply.github.com>
2025-05-19 11:40:35 -0300

    att melhoria

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, AfterViewInit, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
@@ -10,8 +10,13 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit,AfterViewInit {
+scrollToSection(sectionId: string) {
+  const element = document.querySelector(`.${sectionId}`);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
 
   constructor(
     public router: Router
@@ -31,4 +36,49 @@ export class HomeComponent {
   closeNav() {
     this.isNavOpen = false;
   }
+   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    this.checkVisibility();
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.checkVisibility();
+  }
+@ViewChildren('fadeElement') fadeElements!: QueryList<ElementRef>;
+
+  private checkVisibility(): void {
+    const windowHeight = window.innerHeight;
+    
+    this.fadeElements.forEach((elementRef: ElementRef) => {
+      const element = elementRef.nativeElement;
+      const elementTop = element.getBoundingClientRect().top;
+      
+      if (elementTop < windowHeight - 100) {
+        element.classList.add('visible');
+      }
+    });
+  }
+  
+currentSlide = 0;
+totalSlides = 3; // Atualize conforme o número de slides
+
+navigateToDashboard(): void {
+  // Substitua pelo seu método de navegação
+  this.router.navigate(['/dashboard']);
 }
+
+nextSlide(): void {
+  this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+}
+
+prevSlide(): void {
+  this.currentSlide = (this.currentSlide - 1 + this.totalSlides) % this.totalSlides;
+}
+}
+
+
+
+
+  
